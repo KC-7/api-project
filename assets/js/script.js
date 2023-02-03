e675bab424f6f18d604beefad2d5a5c365946a0b
Kieran Collins <kiecol@protonmail.com>
2023-02-03 17:56:17 +0000

    Set up displayException and processOptions functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,35 @@
-const apiKey = "Rozk1lDh49SdTqjtdWi0R65Bke0";
-const apiUrl = "https://ci-jshint.herokuapp.com/api";
+const API_KEY = "Rozk1lDh49SdTqjtdWi0R65Bke0";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
 // ?api_key=Rozk1lDh49SdTqjtdWi0R65Bke0
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
-async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
+function processOptions(form) {
+    let optArray = [];
 
-    const response = await fetch(apiUrl, {
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
+        }
+    }
+
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+}
+
+async function postForm(e) {
+
+    const form = processOptions(new FormData(document.getElementById("checksform")));
+
+    const response = await fetch(API_URL, {
         method: "POST",
         headers: {
-            "Authorization": apiKey,
+            "Authorization": API_KEY,
         },
         body: form,
     });
@@ -22,34 +39,15 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
+
 }
-
-function displayErrors(data) {
-
-    let heading = `JSHint Results for ${data.file}`;
-
-    if (data.total_errors === 0) {
-        results = `<div class="no_errors">No errors reported!</div>`;
-    } else {
-        results = `<div>Total Errors: <span> class="error_count">${data.total_errors}</span></div>`;
-        for (let error of data.error_list) {
-            results += `<div>At line <span class="line">${error.line}</span>, `
-            results += `column <span class="column">${error.col}</span></div>`
-            results += `<div class="error">${error.error}</div>`
-        }
-    }
-
-    document.getElementById("resultsModalTitle").innerText = heading;
-    document.getElementById("results-content").innerHTML = results;
-
-    resultsModal.show();
-}
-
 
 async function getStatus(e) {
-    const queryString = `${apiUrl}?api_key=${apiKey}`;
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
 
@@ -58,8 +56,31 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
+
+}
+
+function displayErrors(data) {
+
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 function displayStatus(data) {
@@ -67,6 +88,20 @@ function displayStatus(data) {
     let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+
+}
+
+function displayException(data) {
+
+    let heading = `An Exception Occured`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error Number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error Text: <strong>${data.error}</strong></div>`;
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
